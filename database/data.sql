c7aa82d1c45e03ff0d0e1724c0ceb8d66a2611dc
Brandon Collette <brandoncollette24@gmail.com>
2021-02-13 11:57:23 -0800

    setup database

---
+++ database/data.sql
@@ -0,0 +1,4 @@
+insert into comments
+    ("name","commentBody","gameId")
+    values('Brandon','This is a great game!!!',1),
+    ('Doomslayer','ehh very glitchy game.',1)
--- database/schema.sql
+++ database/schema.sql
@@ -2,6 +2,45 @@ set client_min_messages to warning;
 
 -- DANGER: this is NOT how to do it in the real world.
 -- `drop schema` INSTANTLY ERASES EVERYTHING.
-drop schema "public" cascade;
+drop schema "gamescore" cascade;
 
-create schema "public";
+create schema "gamescore";
+
+CREATE TABLE "comments" (
+	"commentId" serial NOT NULL,
+	"name" TEXT NOT NULL,
+	"commentBody" TEXT NOT NULL,
+	"gameId" integer NOT NULL,
+	CONSTRAINT "comments_pk" PRIMARY KEY ("commentId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "rating" (
+	"ratingId" serial NOT NULL,
+	"gameId" serial NOT NULL,
+	"rating" integer NOT NULL,
+	CONSTRAINT "rating_pk" PRIMARY KEY ("ratingId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "screenshots" (
+	"screenId" serial NOT NULL,
+	"commentId" integer NOT NULL,
+	"screenUrl" TEXT NOT NULL,
+	CONSTRAINT "screenshots_pk" PRIMARY KEY ("screenId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+
+ALTER TABLE "screenshots" ADD CONSTRAINT "screenshots_fk0" FOREIGN KEY ("commentId") REFERENCES "comments"("commentId");
+
