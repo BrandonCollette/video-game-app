2c218660a31c65d9e47c9ae1901dc182ef8c7ef1
Brandon Collette <brandoncollette24@gmail.com>
2021-02-14 12:26:21 -0800

    Setup routing

--- client/app.jsx
+++ client/app.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom';
 import Home from './pages/home';
+import Search from './pages/search';
 
 export default class App extends React.Component {
   render() {
-    return <Home />;
+    return (
+        <div>
+          <div className="text-center">
+            <Link className="px-4" to="/">Home</Link>
+            <Link className="px-4" to="/search">Search</Link>
+          </div>
+
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+        </div>
+    );
   }
 }
--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 export default function Home(props) {
   return (
     <>
+      <h1 className="text-center">This is Home</h1>
     </>
   );
 }
