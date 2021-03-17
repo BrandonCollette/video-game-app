15bb557ddc8c8cf231b2d106607856ca00c7948a
Brandon Collette <brandoncollette24@gmail.com>
2021-03-16 20:21:31 -0700

    added mobile css

--- client/components/carousel.jsx
+++ client/components/carousel.jsx
@@ -15,7 +15,7 @@ export default function Carousel({ screenshots }){
                     screenshots.map((screenshot,index) => {
                             let cls = (index === 0) ? 'carousel-item active' : 'carousel-item';
                             return (
-                                <div className={ cls } key={index}><img
+                                <div className={ cls } key={index} ><img
                                     src={`https://images.igdb.com/igdb/image/upload/t_1080p/${screenshot.image_id}.jpg`}
                                     className="d-block w-100" alt="image 1"/></div>)
                         }
