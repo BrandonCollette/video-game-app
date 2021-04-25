f53cb04d349c9c3551edc34a8cb91e8dfb98e27d
Brandon Collette <brandoncollette24@gmail.com>
2021-04-25 12:59:24 -0700

    changed some titles on the game pages

--- client/pages/playstation.jsx
+++ client/pages/playstation.jsx
@@ -8,7 +8,7 @@ export default function Playstation(props){
         <>
             <div className="playstationBackground" />
             <Navbar system="playstation" title1="POPULAR PLAYSTATION TITLES" platform1='"fields name,rating,cover.image_id,platforms.name; limit 6; where rating > 80 & platforms = 48;"'
-                    title2="SHOOTER GAMES" platform2='"fields name,rating,cover.image_id,platforms.name; limit 6; where rating > 0 & platforms = 48 & genres = (5,6);"'
+                    title2="SHOOTING GAMES" platform2='"fields name,rating,cover.image_id,platforms.name; limit 6; where rating > 0 & platforms = 48 & genres = (5,6);"'
                     title3="RACING GAMES" platform3='"fields name,rating,cover.image_id,platforms.name; limit 6; where rating > 0 & platforms = 48 & genres = (9,10);"' />
         </>
     );
