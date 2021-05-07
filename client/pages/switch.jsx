277608d0bd71568d998c5a68d650d3327a4917fc
Brandon Collette <brandoncollette24@gmail.com>
2021-05-07 13:52:29 -0700

    Fixed switch page, popular racing games shows the correct amount of games

--- client/pages/switch.jsx
+++ client/pages/switch.jsx
@@ -8,7 +8,7 @@ export default function Switch(props){
         <>
             <div className="switchBackground" />
             <Navbar system="switch" title1="POPULAR SWITCH TITLES" platform1='"fields name,rating,cover.image_id,platforms.name; limit 6; where rating > 80 & platforms = 130 & involved_companies.company = 70;"'
-                    title2="SHOOTING GAMES" platform2='"fields name,rating,cover.image_id,platforms.name; limit 6; where rating > 20 & platforms = 130 & genres = 5;"'
+                    title2="SHOOTING GAMES" platform2='"fields name,rating,cover.image_id,platforms.name; limit 6; where rating > 85 & platforms = (130,4) & genres = 5;"'
                     title3="RACING GAMES" platform3='"fields name,rating,cover.image_id,platforms.name; limit 6; where rating > 0 & platforms = (130,4) & genres = 10;"' />
         </>
     );
